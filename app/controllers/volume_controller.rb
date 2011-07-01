ddd80a997321966dcb65dd72793c5aeecf08245a
HPNeo <hpneo@hotmail.com>
2011-07-01 14:54:52 -0500

    Salida en formato JSON

--- app/controllers/volume_controller.rb
+++ app/controllers/volume_controller.rb
@@ -1,17 +1,23 @@
 class VolumeController < ApplicationController
+	
 	def show
 		@volume = ComicVineVolume.find(params[:volume])
+		render :json => @volume if params[:output]=='json'
 	end
 	
 	def show_issue
 		@volume ||= ComicVineVolume.find(params[:volume])
-		@issue = @volume.issues.select{|issue| issue.issue_number.to_i==params[:issue].to_i }.first
+		issue_number = "#{params[:issue]}.#{params[:format]}"
+		@issue = @volume.issues.select{|issue| issue.issue_number==issue_number.to_s }.first
 		@issue = ComicVineIssue.find(@issue.id)
+		puts params.inspect
+		render :json => @issue if params[:output]=='json'
 	end
 	
 	def search
 		if params[:name]
 			@volumes ||= ComicVineVolume.search(params[:name])
+			render :json => @volumes if params[:output]=='json'
 		end
 	end
 end
